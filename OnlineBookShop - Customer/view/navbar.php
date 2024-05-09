b93f886b38667cc0cdd682840d430986c38994d1
This is Ratul <newidratul@gmail.com>
2024-05-10 01:39:44 +0600

    Update navbar.php

--- OnlineBookShop - Customer/view/navbar.php
+++ OnlineBookShop - Customer/view/navbar.php
@@ -8,9 +8,9 @@
             <!-- ratul: search -->
             <input type="text" name="search">
 
-            <a href="profile.php"><!-- fahim: profile icon --></a>
-            <a href="wishlist.php"><!-- fahim: wishlist icon --></a>
-            <a href="cart.php"><!-- fahim: cart icon --></a>
+            <a href="profile.php">Profile<!-- fahim: profile icon --></a>
+            <a href="wishlist.php">Wishlist<!-- fahim: wishlist icon --></a>
+            <a href="cart.php">Cart<!-- fahim: cart icon --></a>
             <a href=""><!-- ratul: fahim: logout -->Logout</a>
 
             <!-- fahim: call icon --> 1620
