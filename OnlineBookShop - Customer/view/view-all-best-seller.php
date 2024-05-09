885c96a8dd19db2d1ff5eaac708dd77c3b883789
This is Ratul <newidratul@gmail.com>
2024-05-10 03:38:45 +0600

    Update view-all-best-seller.php

--- OnlineBookShop - Customer/view/view-all-best-seller.php
+++ OnlineBookShop - Customer/view/view-all-best-seller.php
@@ -1,46 +1,47 @@
 <?php
-    require('../model/book-model.php');
+require ('../model/book-model.php');
 
-    $best_sellers = get_all_books();
+$best_sellers = get_all_books();
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Best Seller</title>
 </head>
+
 <body>
 
-<!-- fahim: back button  -->
-<a href="customer-home.php">Back</a>
-<?php require_once ('navbar.php') ?>
+    <!-- fahim: back button  -->
+    <a href="customer-home.php">Back</a>
+    <?php require_once ('navbar.php') ?>
     <table class="best-seller-table" id="best-seller-table">
         <tr>
             <td>
                 <h2>Best Seller</h2>
             </td>
         </tr>
-        <tr>
-            <td>
-                Title: <br>
-                Author Name: <br>
-                <img src="" alt="x"><br>
-                Taka: 
-            </td>
-        </tr>
-        <tr>
-            <td>
-                Title: <br>
-                Author Name: <br>
-                <img src="" alt="x"><br>
-                Taka: 
-            </td>
-        </tr>
+        <?php
+        foreach ($best_sellers as $book) {
+            ?>
+                <tr>
+                    <td>
+                        Title: <?= $book['title'] ?><br>
+                        Author Name: <?= $book['author'] ?><br>
+                        <img src="<?= $book['imgdir'] ?>" alt="x"><br>
+                        Taka: <?= $book['price'] ?>
+                    </td>
+                </tr>
+            <?php
+        }
+        ?>
     </table>
-<?php require_once ('footer.php') ?>
+    <?php require_once ('footer.php') ?>
 </body>
+
 </html>
