ed086e4aec1149e4ae12c94b1f2d0951090eed7d
Rianul Amin <87932192+ppsppspsspss@users.noreply.github.com>
2024-05-10 03:27:04 +0600

    .

---
+++ OnlineBookShop - Customer/view/cart.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cart</title>
+</head>
+<body>
+    <!-- fahim: back button  -->
+    <a href="customer-home.php">Back</a>
+    <?php require_once ('navbar.php') ?>
+
+    <!-- fahim: cart table  -->
+    <!-- ratul: cart fetch kor  -->
+    <table class="cart-table" id="cart-table">
+        <tr>
+            <td>
+                <h2>Cart</h2>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Title: <br>
+                Author Name: <br>
+                <img src="" alt="x"><br>
+                Taka: 
+                <!-- ratul: remove koris cart erte  -->
+                <a href="">Remove from Cart</a>
+            </td>
+        </tr>
+    </table>
+    <?php require_once ('footer.php') ?>
+</body>
+</html>
