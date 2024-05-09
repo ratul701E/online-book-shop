eb3de699606036f281009de44e0ba0f363820bcd
Rianul Amin <87932192+ppsppspsspss@users.noreply.github.com>
2024-05-09 23:10:12 +0600

    customer signup

---
+++ OnlineBookShop - Customer/view/sign-up.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body>
+
+    <!-- fahim: back button -->
+    <a href="sign-in.php">Back</a>
+
+    <!-- fahim: registration form -->
+    <form action="" method="post">
+    <table class="registration-table" id="registration-table">
+        <tr>
+            <td>
+                Fullname <br>
+                <input type="text"><br><br>
+                Email <br>
+                <input type="text"><br><br>
+                Phone <br>
+                <input type="text"><br><br>
+                Address <br>
+                <input type="text"><br><br>
+                Username <br>
+                <input type="text"><br><br>
+                Password <br>
+                <input type="text"><br><br>
+                NID <br>
+                <input type="text"><br><br>
+                <!-- fahim: sign up button -->
+                <button class="sign-up-button" id="sign-up-button">Sign Up</button>
+            </td>
+        </tr>
+    </table>
+    </form>
+</body>
+</html>
