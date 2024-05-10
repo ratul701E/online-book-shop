80c90494f1b13ea1cec03e5f3ff41e5be95e516a
Rianul Amin <87932192+ppsppspsspss@users.noreply.github.com>
2024-05-11 03:33:49 +0600

    .

---
+++ OnlineBookShop - Manager/controller/sign-in-controller.php
@@ -0,0 +1,45 @@
+<?php
+require_once '../model/user-model.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Validate 
+
+    if (check_user_existence($username, $password)) {
+        session_start();
+
+        $user = get_user_by_username($username);
+        $_SESSION['user'] = $user;
+        setcookie('username', $username, time() + 3600, '/');
+
+        if($user['role'] == "Admin") {
+            header("Location: ../view/dashboard.php");
+            exit();
+        }
+        else if($user['role'] == "Customer") {
+            //inititate a cart for customer
+            $_SESSION['cart'] = array();
+            header("Location: ../view/customer-home.php");
+            exit();
+        }
+        else if($user['role'] == "Employee") {
+            header("Location: ../view/employee-home.php");
+            exit();
+        }
+        else if($user['role'] == "Manager") {
+            header("Location: ../view/manager-home.php");
+            exit();
+        }
+        else {
+            //
+        }
+
+    } else {
+        header("Location: ../view/sign-in.php?status=1");
+        exit();
+    }
+}
+?>
