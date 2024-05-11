e4ec9581fde3f84947ff3c481901c19118b0ea29
Rianul Amin <87932192+ppsppspsspss@users.noreply.github.com>
2024-05-11 23:51:00 +0600

    .

--- OnlineBookShop - Employee/view/print-report.php
+++ OnlineBookShop - Employee/view/print-report.php
@@ -1,13 +1,34 @@
-    Invoices 
-    Total Earning: <?= $total_money['completed_money'] ?>  TK. 
-    Pending: TK. <?= $total_money['pending_money'] ?> 
-    Overdue: TK. 1000 <!-- ratul: hardcoded -->
-    Essentials: TK. 1000 <!-- ratul: hardcoded -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Print Report</title>
+</head>
+<body>
 
-    <!-- ratul: fetch kor -->
+    <?php require_once('navbar.php') ?>
+    <?php require_once('side-panel.php') ?>
+
+    <!-- ratul: fetch kor ar pdf bana -->
+    <!-- fahim: shundor kor -->
+    Number of Books Available: <?=$counts['total_books'] ?> 
+    Number of Books in Stock: <?=$counts['total_stock'] ?>
+    Number of Books Sold: <?=$counts['total_sold'] ?>
+    Number of Customers: <?=$users['total_customers'] ?>
+    Number of Employees: <?=$users['total_employees'] ?>
+    Number of Managers: <?=$users['total_managers'] ?>
     Total Orders: <?= $orders_count['total_count'] ?>
     Orders Pending: <?= $orders_count['pending_count'] ?>
     Completed Orders: <?= $orders_count['completed_count'] ?>
     Cancelled Orders: <?= $orders_count['cancelled_count'] ?>
-    <?php require_once('navbar.php') ?>
-    <?php require_once('side-panel.php') ?>
+    Total Revenue: TK. <?= $total_money['completed_money'] ?>  
+    Pending: TK. <?= $total_money['pending_money'] ?> 
+    Employee Salary: TK. <?= $total_money['completed_money'] ?>  
+    Profit: TK. <?= $total_money['pending_money'] ?> 
+
+    <button>Print Report</button>
+
+    <?php require_once('footer.php') ?>
+</body>
+</html>
