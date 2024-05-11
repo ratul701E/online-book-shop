45c1243d6cb2813d8fcf58eeb953748ced6b20b2
Rianul Amin <87932192+ppsppspsspss@users.noreply.github.com>
2024-05-11 22:43:50 +0600

    .

--- OnlineBookShop - Employee/view/rating-system.php
+++ OnlineBookShop - Employee/view/rating-system.php
@@ -3,35 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Salary Sheet</title>
+    <title>Rating System</title>
 </head>
 <body>
 <?php require_once('navbar.php') ?>
 <?php require_once('side-panel.php') ?>
 
-    <!-- fahim: salary sheet table -->
+    <!-- fahim: rating system table -->
     <!-- ratul: fetch kor -->
-    <table class="salary-sheet-table" id="salary-sheet-table">
+    <table class="rating-system-table" id="rating-system-table">
         <tr>
             <td colspan="4">
-                Salary Sheet
+                Rating System
             </td>
         </tr>
         <tr>
             <td> ID </td>
             <td> Employees Name </td>
-            <td> Salary </td>
+            <td> Attendance </td>
             <td> Action </td>
         </tr>
         <?php
             foreach($managers as $user) {
                 ?>
                     <tr>
-                        <!-- ratul: salary fetch koris -->
+                        <!-- ratul: attendance fetch koris -->
                         <td> <?= $user['user_id'] ?> </td>
                         <td> <?= $user['full_name'] ?> </td>
-                        <td> salary </td>
-                        <td> <a href="view-profile.php?user_id=<?= $user['user_id'] ?>"> <!-- fahim: view korar icon --></a> </td>
+                        <td> Attendance </td>
+                        <td> <a href=""> Give Attendance </a> </td>
                     </tr>
                 <?php
             }
