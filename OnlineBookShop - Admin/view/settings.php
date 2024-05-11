de103c91937f5485965257cfd35dd8b083846b79
Fahim Wayez <fahimwayez1@gmail.com>
2024-05-12 04:54:39 +0600

    user management complete

--- OnlineBookShop - Admin/view/settings.php
+++ OnlineBookShop - Admin/view/settings.php
@@ -6,19 +6,22 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Management</title>
 </head>
+
 <body>
-<?php require_once('navbar.php') ?>
-<?php require_once('side-panel.php') ?>
+    <?php require_once('navbar.php') ?>
+    <?php require_once('side-panel.php') ?>
 
     <!-- fahim: sobi laga  -->
     <a href="edit-profile-info.php?user_id=<?=$_SESSION['user']['user_id']?>">Edit Profile Info</a>
     <a href="change-password.php">Change Password</a>
 
-<?php require_once('footer.php') ?>
+    <?php require_once('footer.php') ?>
 </body>
+
 </html>
--- OnlineBookShop - Admin/view/user-management.php
+++ OnlineBookShop - Admin/view/user-management.php
@@ -5,20 +5,37 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Management</title>
+    <link rel="stylesheet" href="css/userManagementStyles.css" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
+
 <body>
-<?php require_once('navbar.php') ?>
-<?php require_once('side-panel.php') ?>
+    <?php require_once('navbar.php') ?>
+    <?php require_once('side-panel.php') ?>
 
     <!-- fahim: sobi laga  -->
-    <a href="manage-managers.php">Manager</a>
-    <a href="manage-customers.php">Customer</a>
-    <a href="manage-employees.php">Employee</a>
-
-<?php require_once('footer.php') ?>
+    <div class="container">
+        <a href="manage-managers.php"><span class="material-symbols-outlined">
+                person
+            </span><br>Manager</a>
+        <a href="manage-employees.php"><span class="material-symbols-outlined">
+                support_agent
+            </span><br>Employee</a>
+        <a href="manage-customers.php"><span class="material-symbols-outlined">
+                groups
+            </span><br>Customer</a>
+    </div>
+    <?php require_once('footer.php') ?>
 </body>
+
 </html>
