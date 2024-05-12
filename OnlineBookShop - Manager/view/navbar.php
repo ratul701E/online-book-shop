671541c202d24ab55527fa060f047300d9e3ed81
Rianul Amin <87932192+ppsppspsspss@users.noreply.github.com>
2024-05-12 12:37:50 +0600

    .

---
+++ OnlineBookShop - Manager/view/navbar.php
@@ -0,0 +1,14 @@
+
+<table class="navbar" id="navbar">
+    <tr>
+        <td>
+            <!-- fahim: Home Icon -->
+            <?=$_SESSION['user']['username'] ?> 
+
+            <input type="text" name="search" placeholder="Search Here........">
+
+            <!-- fahim: logo -->
+            <img src="" alt="">
+        </td>
+    </tr>
+</table>
---
+++ OnlineBookShop - Manager/view/right-panel.php
@@ -0,0 +1,15 @@
+<!-- fahim: oije top right er panel da -->
+<table class="right-panel-table" id="right-panel-table">
+    <tr>
+        <td><a href="home.php">Home</a></td>
+    </tr>
+    <tr>
+        <td><a href="employee.php">Employee</a></td>
+    </tr>
+    <tr>
+        <td><a href="candidates.php">Candidates</a></td>
+    </tr>
+    <tr>
+        <td><a href="books.php">Books</a></td>
+    </tr>
+</table>
