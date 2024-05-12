dbc44af3a83cca1cd74e8d4397d7c966c7377420
Rianul Amin <87932192+ppsppspsspss@users.noreply.github.com>
2024-05-12 20:47:50 +0600

    .

--- OnlineBookShop - Manager/view/home.php
+++ OnlineBookShop - Manager/view/home.php
@@ -9,10 +9,11 @@
 <?php require_once('navbar.php') ?>
 <?php require_once('right-panel.php') ?>
 
+<!-- fahim: chhobi ajaira balsal dia bhorayla -->
 <!-- ratul: ajker tarikh fetch kor eine (jigaisna ken) -->
 5 May, 2024
 
-<a href="edit-information.php">Edit Information</a>
+<a href="edit-information.php">Edit Information</a> <!-- fahim: ei 2 da right corner e rakh profile er -->
 <a href="change-password.php">Change Password</a>
 
 <table class="manager-home-table" id="manager-home-table">
