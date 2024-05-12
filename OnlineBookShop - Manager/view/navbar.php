6fad586527614182a89272917b0ede4fb46bbffa
Fahim Wayez <fahimwayez1@gmail.com>
2024-05-13 00:24:33 +0600

    manager navbar complete

--- OnlineBookShop - Manager/view/navbar.php
+++ OnlineBookShop - Manager/view/navbar.php
@@ -1,13 +1,41 @@
-<table class="navbar" id="navbar">
-    <tr>
-        <td>
-            <!-- fahim: Home Icon -->
-            <?=$_SESSION['user']['username'] ?>
+<!DOCTYPE html>
+<html lang="en">
 
-            <input type="text" name="search" placeholder="Search Here........">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/navBarStyles.css" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
 
-            <!-- fahim: logo -->
-            <img src="" alt="">
-        </td>
-    </tr>
-</table>
+<body>
+    <nav>
+        <table class="navbar" id="navbar">
+            <tr>
+                <div class="navBarItems">
+                    <td><button class="navBarUsername"><?=$_SESSION['user']['username'] ?></button></td>
+                    <td>
+                        <div class="search">
+                            <input type="text" name="search" placeholder="Search here...">
+                            <span class="material-symbols-outlined">
+                                search
+                            </span>
+                            <!-- ratul: search -->
+                        </div>
+                    </td>
+                    <td>
+                        <div class="logo">
+                            <!-- fahim: logo -->
+                            <img src="../vendor/logo.png" alt="" height="50px" width="100px">
+                        </div>
+                    </td>
+                </div>
+            </tr>
+
+        </table>
+    </nav>
+</body>
+
+</html>
