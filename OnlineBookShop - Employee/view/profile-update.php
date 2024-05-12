af8576fc4e29a1f9abf2978e7c570d284fa7b9a9
Fahim Wayez <fahimwayez1@gmail.com>
2024-05-12 21:12:48 +0600

    employee complete

--- OnlineBookShop - Employee/view/profile-update.php
+++ OnlineBookShop - Employee/view/profile-update.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile Update</title>
-    <link rel="stylesheet" href="dashboardStyles.css">
+    <link rel="stylesheet" href="css/dashboardStyles.css">
     <script src="../controller/js/script.js"></script>
 </head>
 
@@ -21,70 +21,75 @@
     <?php require_once('side-panel.php') ?>
     <!-- fahim: add employee table -->
     <div class="container">
-        <form action="../controller/add-user-controller.php" method="post"
-            onsubmit="return validateProfileUpdateForm()">
-            <table class="add-employee-table" id="add-employee-table">
+        <div class="dashboardForm">
+
+            <form action="../controller/add-user-controller.php" method="post"
+                onsubmit="return validateProfileUpdateForm()">
+                <table class="add-employee-table" id="add-employee-table">
+                    <tr>
+                        <td>
+                            <h1 align="center">Add Employee</h1>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Fullname <br>
+                            <input type="text" name="full_name"><br><br>
+                            Email <br>
+                            <input type="text" name="email"><br><br>
+                            Mobile Number <br>
+                            <input type="text" name="mobile_number"><br><br>
+                            Address <br>
+                            <textarea type="text" name="Address"> </textarea><br><br>
+                            Username <br>
+                            <input type="text" name="username"><br><br>
+                            Password <br>
+                            <input type="text" name="password"><br><br>
+                            NID <br>
+                            <input type="text" name="nid"><br><br>
+                            <!-- fahim: add employee button -->
+                            <button type="submit">Confirm</button>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+        <div class="dashboardTable">
+            <!-- fahim: employee table -->
+            <table class="employee-table" id="employee-table">
                 <tr>
-                    <td>
-                        <h1 align="center">Add Employee</h1>
+                    <td colspan="6">
+                        <h1 align="center">Profile Update</h1>
                     </td>
                 </tr>
                 <tr>
-                    <td>
-                        Fullname <br>
-                        <input type="text" name="full_name"><br><br>
-                        Email <br>
-                        <input type="text" name="email"><br><br>
-                        Mobile Number <br>
-                        <input type="text" name="mobile_number"><br><br>
-                        Address <br>
-                        <textarea type="text" name="Address"> </textarea><br><br>
-                        Username <br>
-                        <input type="text" name="username"><br><br>
-                        Password <br>
-                        <input type="text" name="password"><br><br>
-                        NID <br>
-                        <input type="text" name="nid"><br><br>
-                        <!-- fahim: add employee button -->
-                        <button type="submit">Confirm</button>
-                    </td>
+                    <th> ID </th>
+                    <th> Employees Name </th>
+                    <th> Status </th>
+                    <th colspan="3"> Action </th>
                 </tr>
-            </table>
-        </form>
-        <!-- fahim: employee table -->
-        <table class="employee-table" id="employee-table">
-            <tr>
-                <td colspan="6">
-                    <h1 align="center">Profile Update</h1>
-                </td>
-            </tr>
-            <tr>
-                <th> ID </th>
-                <th> Employees Name </th>
-                <th> Status </th>
-                <th colspan="3"> Action </th>
-            </tr>
-            <?php
+                <?php
             foreach($employees as $user) {
                 ?>
-            <tr>
-                <td> <?= $user['user_id'] ?> </td>
-                <td> <?= $user['full_name'] ?> </td>
-                <td> <?= $user['status'] ?> </td>
-                <td> <a href="view-profile.php?user_id=<?= $user['user_id'] ?>">View
-                        <!-- fahim: view korar icon -->
-                    </a> </td>
-                <td> <a href="edit-profile-info.php?user_id=<?= $user['user_id'] ?>">Edit
-                        <!-- fahim: edit korar icon -->
-                    </a> </td>
-                <td> <a href="../controller/delete-user-controller.php?user_id=<?= $user['user_id'] ?>">Delete
-                        <!-- fahim: delete korar icon -->
-                    </a> </td>
-            </tr>
-            <?php
+                <tr>
+                    <td> <?= $user['user_id'] ?> </td>
+                    <td> <?= $user['full_name'] ?> </td>
+                    <td> <?= $user['status'] ?> </td>
+                    <td> <a href="view-profile.php?user_id=<?= $user['user_id'] ?>">View
+                            <!-- fahim: view korar icon -->
+                        </a> </td>
+                    <td> <a href="edit-profile-info.php?user_id=<?= $user['user_id'] ?>">Edit
+                            <!-- fahim: edit korar icon -->
+                        </a> </td>
+                    <td> <a href="../controller/delete-user-controller.php?user_id=<?= $user['user_id'] ?>">Delete
+                            <!-- fahim: delete korar icon -->
+                        </a> </td>
+                </tr>
+                <?php
             }
-        ?>
-        </table>
+            ?>
+            </table>
+        </div>
     </div>
     <?php require_once('footer.php') ?>
 </body>
